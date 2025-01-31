674a30093448cc0eb98202d2d03587058dc15d15
Alejandro Soto <alexsotog82@gmail.com>
2025-01-30 22:12:00 -0600

    create notifications microservices

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -5,7 +5,6 @@ import { Logger } from 'nestjs-pino';
 import { ConfigService } from '@nestjs/config';
 import * as cookieParser from 'cookie-parser';
 import { Transport } from '@nestjs/microservices';
-import { config } from 'rxjs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
